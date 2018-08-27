508bc306ddaa639066764ec92f1859f3724c39e7
Guido <guido.vespignani@gmail.com>
2018-08-27 19:15:25 -0300

    Actualizacion angular 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { AdivinaElNumeroComponent } from './componentes/adivina-el-numero/adivina-el-numero.component';
-import { ListadoDeResultadosComponent } from './componentes/listado-de-resultados/listado-de-resultados.component';
-import { LoginComponent } from './componentes/login/login.component';
+import { AdivinaElNumeroComponent } from './components/adivina-el-numero/adivina-el-numero.component';
+import { ListadoDeResultadosComponent } from './components/listado-de-resultados/listado-de-resultados.component';
+import { LoginComponent } from './components/login/login.component';
 //  import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { HttpModule } from '@angular/http';
 
@@ -12,19 +12,19 @@ import { HttpModule } from '@angular/http';
 // agrego las clases para utilizar ruteo
 import { RouterModule, Routes } from '@angular/router';
 
-import { MiHttpService } from './servicios/mi-http/mi-http.service';
-import { PaisesService } from './servicios/paises.service';
+import { MiHttpService } from './services/mi-http/mi-http.service';
+import { PaisesService } from './services/paises.service';
 
-import { JugadoresService } from './servicios/jugadores.service';
-import { ArchivosJugadoresService} from './servicios/archivos-jugadores.service';
-import { ErrorComponent } from './componentes/error/error.component';
-import { PrincipalComponent } from './componentes/principal/principal.component';
-import { AgilidadAritmeticaComponent } from './componentes/agilidad-aritmetica/agilidad-aritmetica.component';
-import { MenuComponent } from './componentes/menu/menu.component';
-import { AdivinaMasListadoComponent } from './componentes/adivina-mas-listado/adivina-mas-listado.component';
-import { AgilidadMasListadoComponent } from './componentes/agilidad-mas-listado/agilidad-mas-listado.component';
-import { RuteandoModule } from './ruteando/ruteando.module';
-import { ListadoComponent } from './componentes/listado/listado.component';
+import { JugadoresService } from './services/jugadores.service';
+import { ArchivosJugadoresService} from './services/archivos-jugadores.service';
+import { ErrorComponent } from './components/error/error.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { AgilidadAritmeticaComponent } from './components/agilidad-aritmetica/agilidad-aritmetica.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AdivinaMasListadoComponent } from './components/adivina-mas-listado/adivina-mas-listado.component';
+import { AgilidadMasListadoComponent } from './components/agilidad-mas-listado/agilidad-mas-listado.component';
+import { RuteandoModule } from './routes/ruteando.module';
+import { ListadoComponent } from './components/listado/listado.component';
 // declaro donde quiero que se dirija
 /*
 const MiRuteo = [{path: 'error' , component: ErrorComponent},
@@ -38,23 +38,23 @@ const MiRuteo = [{path: 'error' , component: ErrorComponent},
 
 {path: '**' , component: ErrorComponent} ];
 */
-import { JugadoresListadoComponent } from './componentes/jugadores-listado/jugadores-listado.component';
+import { JugadoresListadoComponent } from './components/jugadores-listado/jugadores-listado.component';
 
-import { JuegoServiceService } from './servicios/juego-service.service';
-import { ListadosComponent } from './componentes/listados/listados.component';
-import { JuegosComponent } from './componentes/juegos/juegos.component';
-import { RegistroComponent } from './componentes/registro/registro.component';
-import { MenuCardComponent } from './componentes/menu-card/menu-card.component';
-import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
-import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
-import { AnagramaComponent } from './componentes/anagrama/anagrama.component';
-import { ListadoDePaisesComponent } from './componentes/listado-de-paises/listado-de-paises.component';
-import { MapaDeGoogleComponent } from './componentes/mapa-de-google/mapa-de-google.component';
+import { JuegoServiceService } from './services/juego-service.service';
+import { ListadosComponent } from './components/listados/listados.component';
+import { JuegosComponent } from './components/juegos/juegos.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { MenuCardComponent } from './components/menu-card/menu-card.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
+import { AnagramaComponent } from './components/anagrama/anagrama.component';
+import { ListadoDePaisesComponent } from './components/listado-de-paises/listado-de-paises.component';
+import { MapaDeGoogleComponent } from './components/mapa-de-google/mapa-de-google.component';
 import { AgmCoreModule } from '@agm/core';
-import { InputJugadoresComponent } from './componentes/input-jugadores/input-jugadores.component';
+import { InputJugadoresComponent } from './components/input-jugadores/input-jugadores.component';
 import { SexoPipe } from './pipes/sexo.pipe';
-import { NavbarComponent } from './componentes/shared/navbar/navbar.component';
-import { FooterComponent } from './componentes/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
